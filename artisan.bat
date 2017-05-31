46cf6ade070addff539a157611f9aa64a444c0d0
Martin Kleine <Martin Kleine>
2017-05-31 11:17:42 +0200

    optimized dockerfile and batch scripts

--- artisan.bat
+++ artisan.bat
@@ -1,1 +1,1 @@
-docker-compose exec app php artisan %1
+docker-compose exec app php artisan %*
--- initialize.bat
+++ initialize.bat
@@ -1,6 +1,8 @@
-git clone https://github.com/laravel/laravel.git
+docker run --rm -it -w /app -v %cd%:/app travix/base-alpine-git git clone https://github.com/laravel/laravel.git
+
 copy laravel\.env.example laravel\.env
 
 docker run --rm -v %cd%\laravel:/app composer/composer install
 
 docker-compose up -d
+docker-compose exec app php artisan %1
